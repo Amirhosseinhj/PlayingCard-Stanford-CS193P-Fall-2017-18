f0b715a89e71d43abc5e563cb39dadf47f83d7e7
Amirhossein Haji Jafari <amirhjhossein@gmail.com>
2018-10-02 18:22:23 +0330

    Initial Commit

---
+++ PlayingCard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlayingCard
+//
+//  Created by Amirhossein on 10/2/18.
+//  Copyright © 2018 Amirhossein Haji Jafari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
