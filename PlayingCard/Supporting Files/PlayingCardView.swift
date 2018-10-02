6515fcdc45ce665caba91547c8c7d416db43c654
Amirhossein Haji Jafari <amirhjhossein@gmail.com>
2018-10-02 20:07:54 +0330

    5. Drawing Complete

---
+++ PlayingCard/Supporting Files/PlayingCardView.swift
@@ -0,0 +1,19 @@
+//
+//  PlayingCardView.swift
+//  PlayingCard
+//
+//  Created by Amirhossein on 10/2/18.
+//  Copyright © 2018 Amirhossein Haji Jafari. All rights reserved.
+//
+
+import UIKit
+
+class PlayingCardView: UIView {
+
+    
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    
+
+}
